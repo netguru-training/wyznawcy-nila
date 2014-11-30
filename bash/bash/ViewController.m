500c0f03c9b62e9e2d010be2200b852be98d70eb
Radosław Piątek <radoslaw.piatek@netguru.pl>
2014-11-30 16:01:03 +0100

    Clean code a bit.

--- bash/bash/ViewController.m
+++ bash/bash/ViewController.m
@@ -46,12 +46,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void) tableViewDidSelectEntry:(Entry *)entry {
